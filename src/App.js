2366d47ac02362363639f35316dd9a7bf6441363
xhubSupport <support@xhubai.com>
2024-02-20 17:16:51 -0500

    added text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload or whatever v2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
       <h3>Hello World</h3>
+      <>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis lectus arcu, vehicula vulputate purus venenatis laoreet. Aenean molestie tempus cursus. Maecenas sodales ante et sodales eleifend. Nullam fringilla justo ut purus elementum, non tristique lacus ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus at odio vitae ligula volutpat molestie et a urna. Proin fringilla augue id dolor porta, id sagittis lectus semper. Donec vestibulum cursus egestas. Proin ut scelerisque ex. Donec sem tellus, iaculis pellentesque ex eu, auctor suscipit urna. Curabitur eleifend lobortis rhoncus. Phasellus eget tempus eros. Vestibulum vulputate vestibulum nibh, ut rutrum ipsum.</>
     </div>
   );
 }
